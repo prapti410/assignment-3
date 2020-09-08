b21d92541d4f7fa15dbbc7358b8c9818cc07a344
prapti410 <64392255+prapti410@users.noreply.github.com>
2020-09-08 00:24:42 -0700

    Add files via upload

---
+++ q9.py
@@ -0,0 +1,13 @@
+def prime_count(num):
+    count=0
+    for i in range(2,num+1):
+        fact=0
+        for j in range(2,i):
+            if i%j == 0:
+                fact =1
+                break
+                if fact ==0:
+                    count +=1
+            return count
+num=int(input("enter the number"))
+print("there are {prime_count(num)} Prime numbers between 2-{num}")
